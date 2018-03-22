8146c3e491cc751e3e01492b1fc53c77569fa3bc
abc <a@b.de>
2018-03-22 18:29:51 +0100

    Method MayAssign now implemented

--- server/sql_database.go
+++ server/sql_database.go
@@ -2,21 +2,37 @@ package main
 
 import (
 	"database/sql"
+	"time"
 )
 
-const tableNameStudents = "students"
-const tableNameAssignments = "assignments"
-const tableNameProjects = "projects"
+const tableNameStudents = "Students"
+const tableNameAssignments = "Student_in_Project"
+const tableNameProjects = "Projects"
+const tableNameGroups = "Groups"
+const tableNameStudentsInGroups = "Student_in_Group"
+const tableNameFilter = "Filter"
 
-func (c *Connection) Open() {
-	//Opening a Connection to a database
-	db, err := sql.Open("mysql", "") //Depending on with database is going to be used
-	Check(err, true)
-	c.database = db
-}
 
 type Connection struct {
 	database *sql.DB
+}
+
+type Project struct
+{
+	name string
+	description string
+	costs string
+	location string
+	coach string
+	superviser string
+}
+
+
+func (c *Connection) Open() {
+	//Opening a Connection to a database
+	db, err := sql.Open("mysql", "parseTime=true") //Depending on with database is going to be used
+	Check(err, true)
+	c.database = db
 }
 
 func (c *Connection) Close() {
@@ -24,33 +40,48 @@ func (c *Connection) Close() {
 	c.database.Close()
 }
 
-func (c *Connection) addAssignment(student_id int, project_id int) bool {
+func (c *Connection) AddAssignment(studentId int, projectId int, time time.Time, state int) bool {
 	//Add the Assignment to the table via the databseconnection
-	_, err := c.database.Exec("INSERT INTO " + tableNameAssignments + " VALUES(" + string(student_id) + "," + string(project_id) + ");")
+	_, err := c.database.Exec("INSERT INTO " + tableNameAssignments + " VALUES(" + string(studentId) + "," + string(projectId) + ", 0," + string(state) + ");")
 	Check(err, true)
 	return true
 }
 
-func (c *Connection) removeAssignment(student_id int, project_id int) bool {
+func (c *Connection) RemoveAssignment(studentId int, projectId int) bool {
 	//Delete the Assignment in the table via the databseconnection
-	_, err := c.database.Exec("DELDETE FROM " + tableNameAssignments + " WHERE student_id = " + string(student_id) + " AND project_id = " + string(project_id) + ";")
+	_, err := c.database.Exec("DELETE FROM " + tableNameAssignments + " WHERE student_id = " + string(studentId) + " AND project_id = " + string(projectId) + ";")
 	Check(err, true)
 	return true
 }
 
-func (c *Connection) getAssignments(student_id int) string {
-	//Need to know, what coloums should be needed
-	res, err := c.database.Query("SELECT * FROM " + tableNameAssignments + "," + tableNameProjects + " WHERE " + tableNameAssignments + ".project_id = " + tableNameProjects + ".project_id AND student_id = " + string(student_id) + ";")
+func (c *Connection) MayAssign(studentId int, projectId int) bool {
+	//Finds out if a student is allowed to assign to the project
+	res, err := c.database.Query("SELECT isBlacklist FROM " +  tableNameFilter + "," + tableNameProjects + "WHERE id = pid")
 	Check(err, true)
-
-	defer res.Close()
-	for res.Next() {
-		//Read out symbols, that are needed
-		err := res.Scan()
-		Check(err, true)
+	var isBlacklist bool
+	res.Next()
+	err = res.Scan(&isBlacklist)
+	Check(err,true)
+	if !isBlacklist{
+	res, err  = c.database.Query("SELECT COUNT(*) FROM " + tableNameProjects + "," + tableNameGroups + "," + tableNameStudentsInGroups + "," + tableNameFilter + "," + "WHERE " + tableNameProjects + ".id = " + string(projectId) + " AND " + tableNameProjects +".id = " +  tableNameFilter + ".pid AND " + tableNameFilter + ".gid = " + tableNameGroups + ".id AND " +tableNameGroups + ".id = " + tableNameStudentsInGroups + ".gid AND " +  tableNameStudentsInGroups + ".sid = " + string(studentId) + ";")
+	}else{
+	res, err  = c.database.Query("SELECT COUNT(*) FROM " + tableNameProjects + "," + tableNameGroups + "," + tableNameStudentsInGroups + "," + tableNameFilter + "," + "WHERE " + tableNameProjects + ".id = " + string(projectId) + " AND " + tableNameProjects +".id = " +  tableNameFilter + ".pid AND " + tableNameFilter + ".gid <> " + tableNameGroups + ".id AND " +tableNameGroups + ".id = " + tableNameStudentsInGroups + ".gid AND " +  tableNameStudentsInGroups + ".sid = " + string(studentId) + ";")
 	}
-	err = res.Err()
 	Check(err, true)
+	number := checkCount(res)
+	return (number > 0)
+}
 
-	return ""
+func (c *Connection) GetProjectsForStudent(studentId int) []Project {
+	//TODO: Return all Projects that can be chosen by the student
+	return nil
+}
+
+
+func checkCount(rows *sql.Rows) (count int) {
+ 	for rows.Next() {
+    	err:= rows.Scan(&count)
+    	Check(err, true)
+    }   
+    return count
 }
