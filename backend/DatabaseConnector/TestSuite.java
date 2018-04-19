03379a626da4ad8ea2da97f745e936beb57f2ee9
ts <t@s.de>
2018-04-19 20:57:13 +0200

    Refactoring TestSuite

--- backend/DatabaseConnector/TestSuite.java
+++ backend/DatabaseConnector/TestSuite.java
@@ -2,36 +2,46 @@ import java.sql.*;
 import java.util.*;
 import java.io.*;
 
-public class TestProjekte
+public class TestSuite
 {
-    private DatabaseConnection db;
-    public TestProjekte()
-    {
-        db = DatabaseConnection.getDatabaseConnection();
+    private static DatabaseConnection db = DatabaseConnection.getDatabaseConnection();
 
-        try {
-            // Die Datei test.properties enthaelt die sensiblen Zugangsdaten
-            File file = new File("test.properties");
-            FileInputStream fileInput = new FileInputStream(file);
-            Properties properties = new Properties();
-            properties.load(fileInput);
-            fileInput.close();
-
-            String ip = properties.getProperty("IP");           String user = properties.getProperty("USER");   
-            String password = properties.getProperty("PASSWORD");   
-            String database = properties.getProperty("DATABASE");   
-
-            db.configureSettings(ip,user, password, database);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    
-    public void resetDatabase() {
+    public static void resetDatabase() {
         db.executeSQLFile("resetDatabase.sql");         
+    } 
+
+    public static void insertMockupDataStudents() {
+        String s = new String();
+
+        try
+        {
+            FileReader fr = new FileReader(new File("mockupData/students.csv"));
+            BufferedReader br = new BufferedReader(fr);
+
+            String insertInto = "INSERT INTO Students (id, first_name, last_name, password) VALUES (?, ?, ?, ?)";
+            Connection connection = db.getOpenConnection();
+            PreparedStatement statement = connection.prepareStatement(insertInto);
+            while((s = br.readLine()) != null)
+            {
+                s.trim();
+                String[] values = s.split(",");
+                statement.setInt(1, Integer.parseInt(values[0]));
+                statement.setString(2, values[1]);
+                statement.setString(3, values[2]);
+                statement.setString(4, values[3]);
+                statement.addBatch();
+            }
+
+            statement.executeBatch();
+            db.closeConnection();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.toString());
+        }  
     }
 
-    public void insertMockupData()
+    public static void insertMockupDataProjects()
     {
         String s = new String();
         try
@@ -64,7 +74,23 @@ public class TestProjekte
         }
     }
 
-    public void showAllProjects() {
+    public static void showAllStudents() {
+        String sqlCommand = "SELECT * FROM Students;";
+        ResultSet resultSet = db.executeSQLCommand(sqlCommand);
+        try {
+            while(resultSet.next()) {
+                String id = resultSet.getString("id");
+                String first_name = resultSet.getString("first_name");
+                String last_name = resultSet.getString("last_name");
+                String password = resultSet.getString("password");
+                System.out.println(id + "," + first_name + "," + last_name + "," + password);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void showAllProjects() {
         String sqlCommand = "SELECT * FROM Projects;";
         ResultSet resultSet = db.executeSQLCommand(sqlCommand);
         try {
