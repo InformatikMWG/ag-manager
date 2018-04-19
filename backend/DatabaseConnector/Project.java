1f69d51aebfe88a527f33899ac82ba8a0bf19484
flextypes <flextypes@a.de>
2018-04-19 16:04:51 +0200

    Erstenwurf Klasse Projekt

--- backend/DatabaseConnector/Project.java
+++ backend/DatabaseConnector/Project.java
@@ -1,8 +1,31 @@
 import java.util.ArrayList;
 
 public class Project
-{
-    public static ArrayList<Student> getStudentsInProject(int pid)  {
+{    
+    int id;
+    String name; 
+    String description;
+    double costs;
+    String location;
+    String coach;
+    String supervisor;
+    int maxNrStudents;
+
+    public Project(int pid, int id , String name, String description, double costs, String location, String coach, String supervisor, int maxNrStudents)  {
+        id = this.id;
+        name = this.name; 
+        description = this.description;
+        costs = this.costs;
+        location = this.location;
+        coach = this.coach;
+        supervisor = this.supervisor;
+        maxNrStudents = this.maxNrStudents;
+
+    }
+
+    public ArrayList<Project_Slot> getProject_Slots() {
+
         return null;
     }
 }
+
