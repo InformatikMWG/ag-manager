59e6b64a2a0879b87b21f949e7476b0412cefe1c
allgemein <allgemein>
2018-04-23 18:56:05 +0200

    Added Organizer class with method stubs

---
+++ backend/DatabaseConnector/Organizer.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Organizer
+{
+    /**
+     * returns a list of all projects
+     */
+    public static ArrayList<Project> getAllProjects() { return null; }
+
+    /**
+     * returns a list of all students
+     */
+    public static ArrayList<Student> getAllStudents() { return null; }
+
+    /**
+     * returns a list of students which are not enrolled at a specified date
+     */
+    public static ArrayList<Student> getAllStudentsWithoutRegistration(String date) { return null; }
+
+    /**
+     * print out all entry lists
+     * 
+     */
+    public static ArrayList<Project> printAllEntryLists() { return null; }
+    
+}
