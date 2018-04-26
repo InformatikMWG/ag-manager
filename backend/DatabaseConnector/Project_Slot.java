d7061c73662567c25812927c4d770caa5ccd9417
d@h <a@b.de>
2018-04-26 15:52:45 +0200

    removed unnecessary attribute time

--- backend/DatabaseConnector/Project_Slot.java
+++ backend/DatabaseConnector/Project_Slot.java
@@ -2,14 +2,14 @@ public class Project_Slot
 {
     private int pid;
     private String date;
-    private String time;
+    
     private String time_start;
     private String time_end;
 
-    public Project_Slot(int pid, String date, String time, String time_start, String time_end){
+    public Project_Slot(int pid, String date, String time_start, String time_end){
         this.pid = pid;
         this.date = date;
-        this.time = time;
+        
         this.time_start = time_start;
         this.time_end = time_end;                  
     } 
